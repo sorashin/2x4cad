624d17237163bf2a5e9160d80f0fb197c31ec3f9
sorashin <naka52shin@gmail.com>
2025-11-25 12:44:43 +0900

    wip

---
+++ src/components/ui/CommandBar.tsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon";
+import { useUIMode } from "../../hooks/useUIMode";
+
+export function CommandBar() {
+  
+
+  return (
+    <div className="fixed bottom-8 left-8 ">
+      
+    </div>
+  );
+}
